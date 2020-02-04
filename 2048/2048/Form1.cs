fa4c7d9aa7dea847bbd382354a1e81cb93ffdaba
justo202 <justaslabeikis23@gmail.com>
2020-02-04 15:32:39 +0000

    #5

--- 2048/2048/Form1.cs
+++ 2048/2048/Form1.cs
@@ -42,7 +42,74 @@ namespace _2048
 
         private void ButtonEvent_Click(object sender, EventArgs e)
         {
-
+            GenerateNumber(2);
         }
+
+        private void GenerateNumber(int amount)
+        {
+
+            Random random = new Random(); //Generates new number
+            Coordinates coordinates = CheckOpenSpace(); //gets the coordinates of a space
+            if (amount == 1)    //If we need to generate only 1 new number
+            {
+                
+                if(coordinates == null) //if coordinates are null that means there are no more open spaces left
+                {
+                    //GAME OVER (NO MORE SPACE LEFT)
+                    Console.WriteLine("ENDDD");
+                    return;
+                }
+                int number = random.Next(1, 3) == 1 ? 2 : 4; //generate a random number and decide if it's a 2 or a 4
+
+                buttons[coordinates.x, coordinates.y].Text = number.ToString(); //write the new number
+                values[coordinates.x, coordinates.y] = number;
+            }
+            else //if we need 2 numbers
+            {
+                buttons[coordinates.x, coordinates.y].Text = "4"; //write the new number
+                values[coordinates.x, coordinates.y] = 4;
+                coordinates = CheckOpenSpace();
+                buttons[coordinates.x, coordinates.y].Text = "2"; //write the new number
+                values[coordinates.x, coordinates.y] = 2;
+
+            }
+        }
+        
+        private Coordinates CheckOpenSpace()
+        {
+            Coordinates c = null;
+            int count = 0; //the count of free spaces
+            for (int x = 0; x < BOARD_WIDTH; x++)
+            {
+                for (int y = 0; y < BOARD_WIDTH; y++)
+                {
+                   if (values[x, y] == 0) //if it's a free space
+                    {
+                        if(count == 0) //if it's the 1st free space then get it's coordinates
+                        {
+                            c = new Coordinates(x, y);
+                        }
+                        count++;
+                    } 
+                }
+            }
+            if (count > 1) //if there are more then 1 open space available
+            {
+                Random random = new Random();
+                int x, y;
+                do 
+                {
+                    x = random.Next(0, BOARD_WIDTH);
+                    y = random.Next(0, BOARD_WIDTH);
+                } while (values[x, y] != 0); //repeat the loop until an open space is found
+                c = new Coordinates(x, y);
+                               
+            }
+           
+            return c;
+        }
+        
+
     }
+    
 }
